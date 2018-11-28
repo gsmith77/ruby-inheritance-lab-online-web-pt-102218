63c0a4c83481e8e05a02c062342f154671de3848
grant smith <grantsmith1256@gmail.com>
2018-11-28 20:01:44 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,4 +2,14 @@ class User
   
   attr_accessor :first_name, :last_name
   
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+  
 end
+
+ jim = User.new("Jim", "Old")
+ jim.first_name = "Jim"
+ jim.last_name = "Old"
+ 
